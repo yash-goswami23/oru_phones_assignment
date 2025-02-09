86c328e7bf00e8257c78b4c584323913877dd5da
yash-goswami23 <yashuuuu0003@gmail.com>
2025-02-09 14:21:24 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# oru_phones_assignment
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/oru_phones_assignment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.oru_phones_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
